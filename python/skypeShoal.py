0ab395fc4440af74233b3c5f04f808c7e30fe2d8
LarschLabExperimenter <johannes.larsch@gmail.com>
2025-03-16 12:02:20 +0100

    added saving for StimOut csv

--- python/skypeShoal.py
+++ python/skypeShoal.py
@@ -69,7 +69,6 @@ def getAnimalCoors(value,allCoor):
 
 
         i+=1
-
         welldata = ((round(x)), (round(y)), o)
         allCoor.append([x,y,xoff,yoff,o])
         wellstr = "%0.f %0.f %0.3f" % welldata
@@ -83,7 +82,7 @@ def getAnimalCoors(value,allCoor):
     
     # THIS IS QUICK"N DIRTY FOR MAKING MULTI STIMULI WORK
     # 2021-02-17
-    for ea in range(12): #max number of dots
+    for ea in range(8): #max number of dots
     
         #xp = value.Item2.Item2*pxPmm+avgRoiRadius
         #yp = value.Item2.Item3*pxPmm+avgRoiRadius
