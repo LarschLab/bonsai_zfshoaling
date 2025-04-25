4513cebf85c6914400852daa8857f6bae43878c0
LarschLabExperimenter <johannes.larsch@gmail.com>
2025-04-25 10:55:02 +0200

    skypeshoal 8 stim

--- python/skypeShoal.py
+++ python/skypeShoal.py
@@ -82,7 +82,7 @@ def getAnimalCoors(value,allCoor):
     
     # THIS IS QUICK"N DIRTY FOR MAKING MULTI STIMULI WORK
     # 2021-02-17
-    for ea in range(8): #max number of dots
+    for ea in range(12): #max number of dots
     
         #xp = value.Item2.Item2*pxPmm+avgRoiRadius
         #yp = value.Item2.Item3*pxPmm+avgRoiRadius
